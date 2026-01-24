8c143d715d76cf290b8b2b92818c0b14c33b1451
Ajaleshaji <ajaleshbabu@gmail.com>
2026-01-24 13:23:19 +0530

    Intrudeused Multer

--- server/routes/studentFileRoutes.js
+++ server/routes/studentFileRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import upload from "../middleware/upload.js";
+import upload from "../middleware/multer.js";
 import StudentFile from "../models/StudentFile.js";
 import cloudinary from "../config/cloudinary.js";
 
